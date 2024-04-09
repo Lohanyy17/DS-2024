e70b6c08f736b988d6b5ecd9417c6fd6b9122960
Lohany <158223098+Lohanyy17@users.noreply.github.com>
2024-04-09 16:48:36 -0300

    Add files via upload

---
+++ Sistemas Operacionais/Atividades/Atividade2.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001 >nul
+:inicio 
+ipconfig
+set /p inf="Testar conectividade, digite a informação: "
+ping %inf%
+set /p opcao="Deseja continuar? (S/N): "
+
+if "%opcao%"=="S"  (
+	goto inicio
+)
