624d195e790b943f7bdad628bbd490d58ff9c353
Lohany <158223098+Lohanyy17@users.noreply.github.com>
2024-04-09 16:47:58 -0300

    Add files via upload

---
+++ Sistemas Operacionais/Atividades/Atividade1.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001 >nul
+set /p ano = "Digite um ano: "
+mkdir %ano%
+cd 
+mkdir "1 - janeiro"
+mkdir "2 - Fevereiro"
+mkdir "3 - Março"
+mkdir "4 - Abril"
+mkdir "5 - Maio"
+mkdir "6 - Junho"
+mkdir "7 - Julho"
+mkdir "8 - Agosto"
+mkdir "9 - Setembro"
+mkdir "10 - Outubro"
+mkdir "11 - Novembro"
+mkdir "12 - Dezembro" 
+pause
